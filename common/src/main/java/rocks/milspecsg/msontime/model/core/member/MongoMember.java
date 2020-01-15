9b48dde7c0f99f531248fdbb961d520912cfb5d1
allenclan23 <allenclan23@gmail.com>
2020-01-15 09:58:04 -0500

    Fixed missing imports

--- common/src/main/java/rocks/milspecsg/msontime/model/core/member/MongoMember.java
+++ common/src/main/java/rocks/milspecsg/msontime/model/core/member/MongoMember.java
@@ -2,7 +2,7 @@ package rocks.milspecsg.msontime.model.core.member;
 
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
-import rocks.milspecsg.msrepository.model.data.dbo.MongoDbo;
+import rocks.milspecsg.msrepository.common.model.MongoDbo;
 
 import java.util.UUID;
 
